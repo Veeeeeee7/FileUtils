d765991fe59871cd540482747e69162dc3f9eac6
Victor <victorli1076@gmail.com>
2023-08-25 14:30:04 -0700

    File Editor

---
+++ FileWriter/FileEditor.java
@@ -0,0 +1,26 @@
+package FileWriter;
+
+import java.io.File;
+import java.io.FileWriter;
+
+public class FileEditor {
+    public static void createFile(String fileName) {
+        File file = new File("FileWriter/" + fileName + ".txt");
+        try {
+            file.createNewFile();
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+    }
+
+    public static void writeFile(String fileName, String content) {
+        try {
+            File file = new File("FileWriter/" + fileName + ".txt");
+            FileWriter writer = new FileWriter(file);
+            writer.write(content);
+            writer.close();
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+    }
+}
---
+++ FileWriter/Tester.java
@@ -0,0 +1,8 @@
+package FileWriter;
+
+public class Tester {
+    public static void main(String[] args) {
+        FileEditor.createFile("test");
+        FileEditor.writeFile("test", "Hello World!");
+    }
+}
